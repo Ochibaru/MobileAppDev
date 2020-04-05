bc097ff67041dff182a8109c041e496e6e429cd4
ChefSchem <42359209+ChefSchem@users.noreply.github.com>
2020-04-05 12:25:11 -0400

    Files Used for Code Review #2

---
+++ BMI.kt
@@ -0,0 +1,7 @@
+package com.myfitnesstracker.ui.dto
+
+data class BMI(var weight: Double = 0.0, var height: Double = 0.0, var bmi: Double = 0.0, var bmiId: String = "") {
+    override fun toString(): String {
+        return "$bmi"
+    }
+}
